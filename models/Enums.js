48bc26b1a7ca6bcdb178aef1f3b54f898227f8e8
Francisco Fernandez <fraanfeernaandeez@gmail.com>
2025-03-14 09:00:43 +0100

    We created configuration files to properly handle file separation and class implementation.

---
+++ models/Enums.js
@@ -0,0 +1,16 @@
+const ShotResults = {
+    MISS: "MISS",
+    HIT: "HIT",
+    SINK: "SINK",
+};
+
+const CellState = {
+    EMPTY: "EMPTY",
+    SHIP: "SHIP",
+    HIT: "HIT",
+    MISS: "MISS"
+};
+
+// Ejemplo de uso
+console.log(ShotResults.HIT)
+
